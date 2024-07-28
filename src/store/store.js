9558f6252c1faa2c414f31f0e4a285271747dfa0
Pedro M Cruz <pmcruzg@hermenautas.com>
2024-07-28 18:43:19 +0200

    corrige errores en el store de redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import appSlice from '../store/appSlice';
-import chatSlice from '../store/chatSlice';
-import taskKanbanSlice from '../store/taskKanbanSlice';
-import mailSlice from '../store/mailSlice'; // Added in v2.1.0
-import cartSlice from '../store/cartSlice'; // Added in v2.2.0
+import cartSlice from '../store/cartSlice';
 
 export const store = configureStore({
   reducer: {
     app: appSlice,
-    chat: chatSlice,
-    kanban: taskKanbanSlice,
-    mail: mailSlice,
+
     cart: cartSlice,
   },
 });
