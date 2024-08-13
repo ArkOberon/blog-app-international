c0a1f4302406939b6aa4190151f14515ada64e96
Pedro M Cruz <pmcruzg@hermenautas.com>
2024-08-13 22:13:01 +0200

    fix: masonry css files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,7 @@ import Layout from '../components/Layout/Layout';
 import '../styles/scss/theme.scss';
 import '../styles/css/masonry.css';
 
-// Import menssages
+// Import messages
 import messagesEn from '../messages/en.json';
 import messagesEs from '../messages/es.json';
 
