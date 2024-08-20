5099b2ba3a272804dcde02a8c9aac3e766f89e20
xenon <pmcruz@hermenautas.com>
2024-08-20 19:31:27 +0200

    verificacion Google Search Console y Analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,10 @@ export default function Document() {
     <Html>
       <Head>
         <meta name="google" content="notranslate" />
+        <meta
+          name="google-site-verification"
+          content="sV8nJB94bJhXdmMGiOkXs54FdUWHAZ_olhzFTpyXosY"
+        />
       </Head>
       <body>
         <Main />
