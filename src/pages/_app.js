98cb131569eb2b3dde02c8b08b25e87549a44956
xenon <pmcruz@hermenautas.com>
2024-08-20 08:58:49 +0200

    elimina Nextseo del componente _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,6 @@ import React, { Fragment } from 'react';
 // import node module libraries
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { NextSeo } from 'next-seo';
 
 // import provider and store from redux state management.
 import { Provider } from 'react-redux';
@@ -23,10 +22,6 @@ import messagesEs from '../messages/es.json';
 
 function App({ Component, pageProps }) {
   const router = useRouter();
-  const pageURL = process.env.baseURL + router.pathname;
-  const title = 'Hermenautas – Diario de noticias mundial';
-  const description =
-    'Medio de comunicación independiente con información veraz y actualizada, que ofrece las últimas noticas que afectan a las libertades y los derechos de los ciudadanos en todos los ámbitos: economía, tecnología, salud, industria...';
   const keywords =
     'Noticias, Diario, Mundial, Medio de comunicación, Salud, Tecnología, Ciencia, Economía, Industria, Educación, Política, Religión, Investigaciones';
 
@@ -53,25 +48,6 @@ function App({ Component, pageProps }) {
           <meta name="keywords" content={keywords} />
           <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         </Head>
-        <NextSeo
-          title={title}
-          description={description}
-          canonical={pageURL}
-          openGraph={{
-            url: pageURL,
-            title: title,
-            description: description,
-            site_name: process.env.NEXT_PUBLIC_SITE_NAME,
-            images: [
-              {
-                url: '/images/og/og-hermenautas.jpg',
-                width: 1200,
-                height: 630,
-                alt: 'Portada Hermenautas',
-              },
-            ],
-          }}
-        />
         <Provider store={store}>
           <React.StrictMode>
             <Layout>
