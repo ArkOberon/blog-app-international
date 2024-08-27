8a688d163edf833e699833c8f4d62179c5adc3b5
xenon <pmcruz@hermenautas.com>
2024-08-27 12:23:29 +0200

    agrega nueva configuracion para sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,29 +5,24 @@ module.exports = {
   changefreq: 'daily', // Opcional: Frecuencia de cambio de las URLs
   priority: 0.7, // Opcional: Prioridad de las URLs en el sitemap
   sitemapSize: 4000, // Opcional: Tamaño máximo de URLs por archivo sitemap  
-
-  // Opcional: Configurar transformaciones para las URLs
-  transform: async (config, url) => {
-    // Configuración para transformar la URL dependiendo del idioma
-    const locales = ['es', 'en'];
-    const defaultLocale = 'es'; // Cambia esto si "es" no es el idioma predeterminado
-
-    // Genera URLs para todos los idiomas
-    const urls = locales.map(locale => {
-      const isDefaultLocale = locale === defaultLocale;
-      const loc = isDefaultLocale ? `${config.siteUrl}${url}` : `${config.siteUrl}/${locale}${url}`;
-      return {
-        loc, // URL completa
-        changefreq: config.changefreq,
-        priority: config.priority,
-        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-        alternateRefs: locales.map((altLocale) => ({
-          href: altLocale === defaultLocale ? `${config.siteUrl}${url}` : `${config.siteUrl}/${altLocale}${url}`,
-          hreflang: altLocale,
-        })),
-      };
-    });
-
-    return urls;
+  alternateRefs: [
+    {
+      href: 'https://hermenautas.com/',
+      hreflang: 'es',
+    },
+    {
+      href: 'https://hermenautas.com/en/',
+      hreflang: 'en',
+    },
+  ],
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
   },
+  
 }
